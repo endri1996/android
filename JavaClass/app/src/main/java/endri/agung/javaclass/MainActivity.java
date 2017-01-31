b1432e649081d9a20d21bca463bd8061fd83739b
unknown <agungpambudi583@gmail.com>
2017-01-31 10:08:11 +0700

    biodata diri

--- JavaClass/app/src/main/java/endri/agung/javaclass/MainActivity.java
+++ JavaClass/app/src/main/java/endri/agung/javaclass/MainActivity.java
@@ -2,12 +2,28 @@ package endri.agung.javaclass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public String showString;
+    public TextView showText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showText =(TextView) findViewById(R.id.show_text);
+
+        barang newbarang = new barang();
+        newbarang.setNama("laptop")
+        String nmbarang = newbarang.getNama();
+
+        showString = " Manipulasi Class Java Android";
+        addSeparator();
+        showText.setText(showString);
+    }
+    public void addSeparator(){
+        showString += "\n........................................................\n";
     }
 }
---
+++ JavaClass/app/src/main/java/endri/agung/javaclass/barang.java
@@ -0,0 +1,17 @@
+package endri.agung.javaclass;
+
+/**
+ * Created by D2J-00 on 31/01/2017.
+ */
+public class barang {
+    private String nama;
+    private int category;
+    private int harga;
+
+    public void setNama(String nama){
+        this.nama=nama;
+    }
+    public String getNama(){
+        return this.nama;
+    }
+}
