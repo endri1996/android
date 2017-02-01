acb5ccc9f6b85f24fda08c6edda7c13ec9d369f0
endri1996 <agungpambudi583@gmail.com>
2017-02-01 08:27:01 +0700

    Java Class

--- JavaClass/app/src/main/java/endri/agung/javaclass/transaksi.java
+++ JavaClass/app/src/main/java/endri/agung/javaclass/transaksi.java
@@ -1,7 +1,42 @@
 package endri.agung.javaclass;
 
+import java.util.ArrayList;
+
 /**
  * Created by D2J-00 on 31/01/2017.
  */
 public class transaksi {
+    private ArrayList<barang> Listbarang =new ArrayList<>();
+
+
+    public void addbarang(barang barang)
+    {
+        this.Listbarang.add(barang);
+    }
+     public int totaltransaksi(){
+         int hasil = 0;
+         for(int i=0; i < Listbarang.size();i++)
+         {
+             hasil +=Listbarang.get(i).getHarga();
+         }
+         return hasil;
+     }
+
+    public String printtransaksi(){
+        String text = "barang yang dibeli pada transasksi adalah    :\n";
+        text +="\n...................................................\n";
+
+        for(int i= 0; i<Listbarang.size();i++)
+        {
+            text += Listbarang.get(i).toString();
+        }
+        text += "\n..................................................\n";
+        text += "total pembelian:"+totaltransaksi()+"\n";
+        text += "\n...................................................\n";
+        return text;
+    }
+
+
+
+
 }
